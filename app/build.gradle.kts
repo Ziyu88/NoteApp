24af7197cd59c38baf803abebefca5addea57532
Ziyu88 <y52112@outlook.com>
2025-12-29 23:35:00 +0800

    Merge remote-tracking branch 'origin/master'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
 
     // 添加这行来解决问题
     implementation("androidx.work:work-runtime:2.9.0")
+
+    // Kotlin metadata support (>= 2.1.0)
+    implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.9.0")
 }
